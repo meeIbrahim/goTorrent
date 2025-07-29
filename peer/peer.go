b09b0383d5b5ab10d4b29b71daf5a10393b2ab3c
Ibrahim <ibrahim@arch.zephyrus.local>
2025-07-29 23:58:01 +0500

    Add: Handshake to peer package

--- peer/peer.go
+++ peer/peer.go
@@ -3,12 +3,58 @@ package peer
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 )
 
 type Peer struct {
 	IP   net.IP
 	Port uint16
+}
+
+type Handshake struct {
+	Pstr     string
+	InfoHash [20]byte
+	PeerID   [20]byte
+}
+
+func (h *Handshake) Serialize() []byte {
+	buf := make([]byte, len(h.Pstr)+49)
+	buf[0] = byte(len(h.Pstr))
+	curr := 1
+	curr += copy(buf[curr:], []byte(h.Pstr))
+	curr += copy(buf[curr:], make([]byte, 8))
+	curr += copy(buf[curr:], h.InfoHash[:])
+	curr += copy(buf[curr:], h.PeerID[:])
+	return buf
+}
+
+func ReadHandShake(r io.Reader) (*Handshake, error) {
+	lengthBuf := make([]byte, 1)
+	_, err := io.ReadFull(r, lengthBuf)
+	if err != nil {
+		return nil, err
+	}
+	length := int(lengthBuf[0])
+	if length == 0 {
+		err := fmt.Errorf("Protocol Length cannot be 0")
+		return nil, err
+	}
+	handshakeBuf := make([]byte, length+48)
+
+	_, err = io.ReadFull(r, handshakeBuf)
+	if err != nil {
+		return nil, err
+	}
+	var infoHash, peerID [20]byte
+	copy(infoHash[:], handshakeBuf[length+8:length+8+20])
+	copy(peerID[:], handshakeBuf[length+8+20:])
+	h := Handshake{
+		Pstr:     string(handshakeBuf[0:length]),
+		InfoHash: infoHash,
+		PeerID:   peerID,
+	}
+	return &h, nil
 }
 
 func Unmarshal(peersBin []byte) ([]Peer, error) {
